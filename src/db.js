eee6d69e7ac6295aa5e4aaefa2d229d3c212bf07
Stylessh <danicomegalletas5@gmail.com>
2019-04-20 19:38:09 -0400

    adding mongodbAtlas

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/Node-completeApp-DB', {
+mongoose.connect('mongodb+srv://stylessh:admindb@cluster0-6hd5v.mongodb.net/test?retryWrites=true', {
     useNewUrlParser: true
 })
     .then(db => { console.log('DB is connected')})
